3619e460dd31e77b86677341b57daec0e2344b58
Rong <rongster3544@gmail.com>
2018-01-18 18:12:08 -0800

    updated package.json and webpack.config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Root = () => {
+  return (
+    <div> hi </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
