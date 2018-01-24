5b0845dabfbfc8c0aaad082244ec14192b7074ce
Rong <rongster3544@gmail.com>
2018-01-23 23:58:51 -0800

    gitignore

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 // this logs the requests
 app.use((req, res, next) => {
-  // console.log(req.method, req.url);
+  console.log(req.method, req.url); // eslint-disable-line no-console
   next();
 });
+
 app.use(bodyParser.text());
 
 // this sets up routes for the other files
 app.use('/static', express.static(path.join(__dirname, '/../')));
+
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, '/../bulb.png'));
 });
@@ -21,11 +24,5 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../index.html'));
 });
 
-// app.set('host', process.env.HOST || 'localhost');
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'));
-
-// app.listen(app.get('port'), app.get('host'), function(error) {
-//   if(error) console.log('error');
-//   console.log(`Server listening @ ${app.get('host')}:${app.get('port')}`);
-// });
